4c1a418cd1e1cf8068aa44d7676f0f180e38e16b
Philipp Bosch <hello@pb.io>
2019-07-08 12:11:40 +0200

    feat: cache calculated filter values.

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const envPaths = require('env-paths');
+
 const {
   Color,
   Solver,
   hexToRgb
 } = require('./utils');
+
+const cacheDir = envPaths('tailwindcss-colorize').cache;
+const cacheFilePath = path.join(cacheDir, 'cache.json');
+
+if (!fs.existsSync(cacheDir)) {
+  fs.mkdirSync(cacheDir);
+}
+
+function readCache() {
+  try {
+    const jsonData = fs.readFileSync(cacheFilePath, 'utf8');
+    const data = JSON.parse(jsonData);
+    return data;
+  } catch (e) {
+    fs.writeFileSync(cacheFilePath, JSON.stringify({}), 'utf8');
+    return {};
+  }
+}
+
+function writeCache(data) {
+  fs.writeFileSync(cacheFilePath, JSON.stringify(data), 'utf8');
+}
+
 
 module.exports = function colorize({
   addUtilities,
@@ -13,15 +40,26 @@ module.exports = function colorize({
   const variants = config('variants.colorize', []);
   const utilities = {};
 
+  const cache = readCache();
+
   colorNames.forEach(colorName => {
     const dashIndex = colorName.lastIndexOf('-');
     const baseColor = colorName.substr(0, dashIndex);
     const weight = colorName.substr(dashIndex + 1);
     const hexColor = config(`theme.colors.${baseColor}[${weight}]`);
-    const rgbValues = hexToRgb(hexColor);
-    const color = new Color(...rgbValues);
-    const solver = new Solver(color);
-    const result = solver.solve();
+    let result;
+    if (hexColor in cache) {
+      console.debug(`Result for ${hexColor} successfully found in cache`);
+      result = cache[hexColor];
+    } else {
+      console.debug(`Result for ${hexColor} NOT found in cache`);
+      const rgbValues = hexToRgb(hexColor);
+      const color = new Color(...rgbValues);
+      const solver = new Solver(color);
+      result = solver.solve();
+      cache[hexColor] = result;
+      writeCache(cache);
+    }
 
     utilities[`.colorize-${e(colorName)}`] = {
       'filter': result.filter.split(': ')[1].replace(/;$/, ''),
